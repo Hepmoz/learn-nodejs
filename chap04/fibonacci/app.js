cdd39274318a847970cace35987de55890864528
Héctor Pérez <hector.perez.m@protonmail.com>
2018-10-11 19:08:13 -0300

    Chap04 finished

--- chap04/fibonacci/app.js
+++ chap04/fibonacci/app.js
@@ -3,15 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// const fibonacci = require('./routes/fibonacci');
+//const fibonacci = require('./routes/fibonacci-async1');
+const fibonacci = require('./routes/fibonacci-rest');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/fibonacci', fibonacci);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
